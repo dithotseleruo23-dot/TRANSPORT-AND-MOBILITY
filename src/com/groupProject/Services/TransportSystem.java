bcee149ce658285be828bf7d9559bbbe63658aca
BaratiSteward <stewardmoapare@gmail.com>
2026-04-28 01:36:05 +0200

    Update TransportSystem.java
    Adedd TransportSystem with route management (addRoute, getRoute)

--- src/com/groupProject/Services/TransportSystem.java
+++ src/com/groupProject/Services/TransportSystem.java
@@ -1,19 +1,37 @@
 package com.groupProject.Services;
+
 import java.util.List;
-
-import com.groupProject.Travel.Route;
-
 import java.util.ArrayList;
+import com.groupProject.Travel.Route;
+import com.groupProject.Transportation.TransportMedium;
 
 public class TransportSystem {
     private List<Route> routes = new ArrayList<>();
+    private List<TransportMedium> transports = new ArrayList<>();
 
-    public void addRoute(Route route){
+    public void addRoute(Route route) {
         routes.add(route);
     }
 
-    public List<Route> getRoutes(){
+    public List<Route> getRoutes() {
         return routes;
     }
     
+    public void addTransport(TransportMedium transport) {
+        transports.add(transport);
+    }
+    
+    public List<TransportMedium> getTransports() {
+        return transports;
+    }
+    
+    public List<TransportMedium> getAvailableTransports() {
+        List<TransportMedium> available = new ArrayList<>();
+        for (TransportMedium t : transports) {
+            if (t.isAvailable()) {
+                available.add(t);
+            }
+        }
+        return available;
+    }
 }
