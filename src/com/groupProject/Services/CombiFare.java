2935d734a19f829e8529d33758cfb1d7ac302dcb
BaratiSteward <stewardmoapare@gmail.com>
2026-04-29 13:38:20 +0200

    Update CombiFare.java

--- src/com/groupProject/Services/CombiFare.java
+++ src/com/groupProject/Services/CombiFare.java
@@ -1,6 +1,6 @@
 package com.groupProject.Services;
 
-public class CombiFare implements fareCalculator {
+public class CombiFare implements farecalculator {
     private double combiPrice;
 
     public CombiFare(double combiPrice){
