5f836f5226ef902bbf477d8c6eb72c71c7f31b42
BaratiSteward <stewardmoapare@gmail.com>
2026-04-30 21:49:16 +0200

    Update InvalidLocationException.java

--- src/com/groupProject/Exceptions/InvalidLocationException.java
+++ src/com/groupProject/Exceptions/InvalidLocationException.java
@@ -1,1 +1,8 @@
+package com.groupProject.Exceptions;
 
+public class InvalidLocationException extends Exception {
+
+    public InvalidLocationException(String message) {
+        super(message);
+    }
+}
