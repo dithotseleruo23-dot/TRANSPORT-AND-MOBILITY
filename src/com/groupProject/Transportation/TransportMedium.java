fcefd622d7f87258872f484438e65210bd2f4bf5
BaratiSteward <stewardmoapare@gmail.com>
2026-04-30 13:52:02 +0200

    Update TransportMedium.java
    incorporated exception handling to transport medium through invalidexception fare

--- src/com/groupProject/Transportation/TransportMedium.java
+++ src/com/groupProject/Transportation/TransportMedium.java
@@ -1,6 +1,7 @@
 package com.groupProject.Transportation;
 
 import com.groupProject.Services.fareCalculator;
+import com.groupProject.Exceptions.InvalidFareException;
 
 public abstract class TransportMedium implements fareCalculator {
 
@@ -23,26 +24,27 @@ public abstract class TransportMedium implements fareCalculator {
     // Abstract method - subclasses must provide their own fare logic
     public abstract double calculateFare(double distance);
 
-
     // Getters
     public String getName() { return name; }
     public String getRouteName() { return routeName; }
     public double getBaseFare() { return baseFare; }
-    public boolean isAvailable(){
-        return isAvailable;
-    }
+    public boolean isAvailable() { return isAvailable; }
 
     // Setters
-    public void setAvailable(boolean available) { 
-        this.isAvailable = available; 
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
     }
-    public void setRouteName(String routeName) { 
-        this.routeName = routeName; 
+
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
     }
-    public void setBaseFare(double FARE) {
-        if (FARE >= 0) {
-            this.baseFare = FARE;
+
+    // Throws InvalidFareException if a negative fare is passed
+    public void setBaseFare(double FARE) throws InvalidFareException {
+        if (FARE < 0) {
+            throw new InvalidFareException("Fare cannot be negative: " + FARE);
         }
+        this.baseFare = FARE;
     }
 
     public void displayInfo() {
