48968dd36a07bcbc177a5e8cf1316efe9e0b75dd
BaratiSteward <stewardmoapare@gmail.com>
2026-04-30 21:30:22 +0200

    Update Route.java
    Added distance field to the Route class so that it automatically calculates distance for the user to see

--- src/com/groupProject/Travel/Route.java
+++ src/com/groupProject/Travel/Route.java
@@ -3,7 +3,6 @@ package com.groupProject.Travel;
 import com.groupProject.Transportation.TransportMedium;
 import java.util.ArrayList;
 
-
 public class Route {
 
     private String routeName;
@@ -11,13 +10,16 @@ public class Route {
     private Location destination;
     private TransportMedium vehicle;
     private ArrayList<Location> stops;
+    private double distance; // km - used for automatic fare calculation
 
-    public Route(Location start, String routeName, TransportMedium vehicle, Location destination) {
+    // Constructor now takes distance so each route knows how long it is
+    public Route(Location start, String routeName, TransportMedium vehicle, Location destination, double distance) {
         this.start = start;
         this.routeName = routeName;
         this.vehicle = vehicle;
         this.destination = destination;
         this.stops = new ArrayList<Location>();
+        this.distance = distance;
     }
 
     public void addStop(Location location) {
@@ -28,17 +30,20 @@ public class Route {
         return stops.isEmpty();
     }
 
+    // Getters
     public String getRouteName() { return routeName; }
     public Location getStart() { return start; }
     public Location getDestination() { return destination; }
     public TransportMedium getVehicle() { return vehicle; }
     public ArrayList<Location> getStops() { return stops; }
+    public double getDistance() { return distance; }
 
     public void displayInfo() {
         System.out.println("Route    : " + routeName);
         System.out.println("From     : " + start.getPlace());
         System.out.println("To       : " + destination.getPlace());
         System.out.println("Vehicle  : " + vehicle.getName());
+        System.out.println("Distance : " + distance + " km");
         System.out.println("Direct   : " + (isDirectRoute() ? "Yes" : "No"));
     }
 }
